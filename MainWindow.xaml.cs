14aa0afbccb6ab75e4cac783fdaa06b44c8eb70f
tanzfisch <martinloga@protonmail.com>
2019-04-13 09:48:21 +1200

    game timer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ScoreCounter
 {
@@ -20,9 +21,70 @@ namespace ScoreCounter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timerGameTime;
+        TimeSpan gameTime;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            setGameTime(TimeSpan.FromMinutes(5));
+        }
+
+        private void Button_Click300(object sender, RoutedEventArgs e)
+        {
+            setGameTime(TimeSpan.FromMinutes(3));
+        }
+
+        private void Button_Click500(object sender, RoutedEventArgs e)
+        {
+            setGameTime(TimeSpan.FromMinutes(5));
+        }
+
+        private void Button_Click700(object sender, RoutedEventArgs e)
+        {
+            setGameTime(TimeSpan.FromMinutes(7));
+        }
+
+        private void Button_Click1000(object sender, RoutedEventArgs e)
+        {
+            setGameTime(TimeSpan.FromMinutes(10));
+        }
+
+        private void Button_Click2000(object sender, RoutedEventArgs e)
+        {
+            setGameTime(TimeSpan.FromMinutes(20));
+        }
+
+        private void Button_Click2500(object sender, RoutedEventArgs e)
+        {
+            setGameTime(TimeSpan.FromMinutes(25));
+        }
+
+        private void setGameTime(TimeSpan timeSpan)
+        {
+            gameTime = timeSpan;
+            updateGametimeLabel();
+        }
+
+        private void updateGametimeLabel()
+        {
+            labelGameTime.Content = String.Format("{0,2:00}:{1,2:00}", gameTime.Minutes, gameTime.Seconds);
+        }
+
+        private void Button_ClickStart(object sender, RoutedEventArgs e)
+        {
+            timerGameTime = new DispatcherTimer(new TimeSpan(0, 0, 0, 0, 100), DispatcherPriority.Normal, delegate
+            {
+                updateGametimeLabel();
+                if (gameTime == TimeSpan.Zero)
+                {
+                    timerGameTime.Stop();
+                }
+                gameTime = gameTime.Add(TimeSpan.FromMilliseconds(-100));
+            }, Application.Current.Dispatcher);
+
+            timerGameTime.Start();
         }
     }
 }
